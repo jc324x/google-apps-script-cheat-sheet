e2e25b2113d881e08cfc996a26ff9b1ea93a2d0e
Jon Christensen <jon@jychri.com>
2018-12-23 12:44:15 -0600

    copy-all-the-things

--- copy-spreadsheet.js
+++ copy-spreadsheet.js
@@ -1,47 +1,174 @@
 function verifyCloneAtPath(path, url) {
     var folderPath = getInverseBasename(path);
-    verifyFolderPath(folderPath);
-    var mime = mimeFromURL(url);
+    var fldr = verifyFolderPath(folderPath);
+    var name = getBasename(folderPath);
+    var mime = mimeFromUrl(url);
 
     if (checkForFileAtPath(path, mime)) {
         return findFileAtPath(path, mime);
-    } else {
-        // this is where the clone would happen
+    }
+
+    var id;
+
+    switch (mime) {
+        case "spreadsheet":
+            id = SpreadsheetApp.openByUrl(url).getId();
+            break;
+        case "document":
+            id = DocumentApp.openByUrl(url).getId();
+            break;
+        case "spreadsheet":
+            id = SlidesApp.openByUrl(url).getId();
+            break;
+        case "form":
+            id = FormApp.openByUrl(url).getId();
+            break;
+    }
+
+    var file = DriveApp.getFileById(id);
+
+    if (file.getId() != "") {
+        file.makeCopy(name, fldr);
     }
 }
 
 function mimeFromUrl(url) {
-    switch (url) {
-        case url.includes("spreadsheets"):
-            return "spreadsheet";
-        case url.includes("document"):
-            return "document";
-    }
-}
-
-function cloneByUrl(url) {
-    var pub;
-    var name;
-
     if (checkStringForSubstring("spreadsheets", url)) {
-        pub = SpreadsheetApp.openByUrl(url);
-        Logger.log(pub.getName());
-        Logger.log(pub.getId());
+        return "spreadsheet";
     }
 
+    if (checkStringForSubstring("document", url)) {
+        return "document";
+    }
 
-    // switch (url) {
-    //     case checkStringForSubstring("google", url) == true:
-    //         Logger.log("case started");
-    //         var copy = SpreadsheetApp.openByUrl(url).makeCopy();
-    //         Logger.log(copy.getName());
-    //         // name = pub.getName();
-    //         // break;
-    //         // case url.includes("document"):
-    //         // pub = DocumentApp.openByUrl(url);
-    //         // break;
-    // }
+    if (checkStringForSubstring("presentation", url)) {
+        return "presentation";
+    }
+
+    if (checkStringForSubstring("forms", url)) {
+        return "form";
+    }
+
 }
+
+function verifyFolderPath(path) {
+    path = validatePathString(path);
+    var split = path.split("/");
+    var fldr;
+    for (i = 0; i < split.length; i++) {
+        var fi = DriveApp.getRootFolder().getFoldersByName(split[i]);
+        if (i === 0) {
+            if (!(fi.hasNext())) {
+                DriveApp.createFolder(split[i]);
+                fi = DriveApp.getFoldersByName(split[i]);
+            }
+            fldr = fi.next();
+        } else if (i >= 1) {
+            fi = fldr.getFoldersByName(split[i]);
+            if (!(fi.hasNext())) {
+                fldr.createFolder(split[i]);
+                fi = DriveApp.getFoldersByName(split[i]);
+            }
+            fldr = fi.next();
+        }
+    }
+    return fldr;
+}
+
+function checkArrayForValue(arr, val) {
+    return arr.indexOf(val) > -1;
+}
+
+function findFolderInFolder(name, fldr) {
+    var fldrs = arrayOfFoldersInFolder(fldr);
+    var names = arrayOfFolderNames(fldrs);
+    if (checkArrayForValue(names, name)) {
+        return fldr.getFoldersByName(name).next();
+    } else {
+        return false;
+    }
+}
+
+function findFolderAtPath(path) {
+    path = validatePathString(path);
+    var fi, fldr;
+    var split = path.split("/");
+
+    for (i = 0; i < split.length; i++) {
+        if (i === 0) {
+            fi = DriveApp.getRootFolder().getFoldersByName(split[i]);
+            if (fi.hasNext()) {
+                fldr = fi.next();
+            } else {
+                return false;
+            }
+        } else if (i >= 1) {
+            fi = fldr.getFoldersByName(split[i]);
+            if (fi.hasNext()) {
+                fldr = fi.next();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    var target = getBasename(path);
+    if (fldr.getName() === target) {
+        return fldr;
+    } else {
+        return false;
+    }
+}
+
+function getBasename(path) {
+    path = validatePathString(path);
+    var split = path.split("/");
+    return split.pop();
+}
+
+
+function validateMIME(val) {
+    val = val.toLowerCase();
+    var prefix = "application/vnd.google-apps.";
+    var mimes = [
+        "audio", "document", "drawing",
+        "drive-sdk", "file", "folder",
+        "form", "fusiontable", "map",
+        "photo", "presentation", "script",
+        "site", "spreadsheet", "unknown", "video"
+    ];
+    for (var i = 0; i < mimes.length; i++) {
+        var mime = mimes[i];
+        if (val === prefix.concat(mime)) {
+            return val;
+        }
+    }
+    if (checkArrayForValue(mimes, val)) {
+        return prefix.concat(val);
+    } else {
+        return false;
+    }
+}
+
+function validatePathString(path) {
+    if ((path.charAt(0)) === "/") {
+        path = path.substr(1);
+    }
+    if ((path.charAt(path.length - 1) === "/")) {
+        path = path.slice(0, -1);
+    }
+    return path;
+}
+
+
+
+function getInverseBasename(path) {
+    path = validatePathString(path);
+    var split = path.split("/");
+    split.pop();
+    return split.join("/");
+}
+
 
 function checkStringForSubstring(val, str) {
     if (str.indexOf(val) > -1) {
@@ -51,10 +178,158 @@ function checkStringForSubstring(val, str) {
     }
 }
 
+
+function arrayOfFolderNames(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        var name = arr[i].getName();
+        result.push(name);
+    }
+    return result;
+}
+
+
+function arrayOfFoldersInFolder(fldr) {
+    var result = [];
+    var fi = fldr.getFolders();
+    while (fi.hasNext()) {
+        var _fldr = fi.next();
+        result.push(_fldr);
+    }
+    return result;
+}
+
+
+
+function findFolderInFolder(name, fldr) {
+    var fldrs = arrayOfFoldersInFolder(fldr);
+    var names = arrayOfFolderNames(fldrs);
+    if (checkArrayForValue(names, name)) {
+        return fldr.getFoldersByName(name).next();
+    } else {
+        return false;
+    }
+}
+
+
+function arrayOfFileNames(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        var name = arr[i].getName();
+        result.push(name);
+    }
+    return result;
+}
+
+function arrayOfFilesInFolder(fldr) {
+    var result = [];
+    var fi = fldr.getFiles();
+    while (fi.hasNext()) {
+        var file = fi.next();
+        result.push(file);
+    }
+    return result;
+}
+
+
+function findFileInFolder(name, fldr, mime) {
+
+    function findFileInFolderAny(name, fldr) {
+        var files = arrayOfFilesInFolder(fldr);
+        var names = arrayOfFileNames(files);
+        if (checkArrayForValue(names, name)) {
+            return fldr.getFilesByName(name).next();
+        } else {
+            return false;
+        }
+    }
+
+    function findFileInFolderType(name, fldr, mime) {
+        mime = validateMIME(mime);
+        if (mime) {
+            var files = arrayOfFilesInFolder(fldr);
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                if ((file.getName() === name) && file.getMimeType() === mime) {
+                    return file;
+                }
+            }
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    if (mime !== undefined) {
+        return findFileInFolderType(name, fldr, mime);
+    } else {
+        return findFileInFolderAny(name, fldr);
+    }
+}
+
+
+function findFileAtPath(path, mime) {
+
+    function findFileAtPathAny(path) {
+        path = validatePathString(path);
+        var file = getBasename(path);
+        path = getInverseBasename(path);
+        var fldr = findFolderAtPath(path);
+        if (fldr) {
+            return findFileInFolder(file, fldr);
+        } else {
+            return false;
+        }
+    }
+
+    function findFileAtPathType(path, mime) {
+        path = validatePathString(path);
+        mime = validateMIME(mime);
+        var file = getBasename(path);
+        path = getInverseBasename(path);
+        var fldr = findFolderAtPath(path);
+
+        if (fldr) {
+            file = findFileInFolder(file, fldr);
+        } else {
+            return false;
+        }
+
+        if (file && matchMIMEType(file, mime)) {
+            return file;
+        } else {
+            return false;
+        }
+    }
+
+    if (mime !== undefined) {
+        return findFileAtPathType(path, mime);
+    } else {
+        return findFileAtPathAny(path);
+    }
+}
+
+
+
+function checkForFileAtPath(path, mime) {
+    var check = findFileAtPath(path, mime);
+    if (check) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 // here we go
 
 function testing() {
-    cloneByUrl("https://docs.google.com/spreadsheets/d/1d0M8CnuRBLvhLU5R8vq_rfc4jLkrxcsgKITfodm4U7s/edit#gid=0");
+    verifyCloneAtPath("qs/sheets/data-sheet", "https://docs.google.com/spreadsheets/d/1d0M8CnuRBLvhLU5R8vq_rfc4jLkrxcsgKITfodm4U7s/edit#gid=0");
+  // FLAG: write a way to validate Files before working with them. Verify that the have Ids and valid URLs. test ownership too? idk
+  //
+  //
     // var url = "https://docs.google.com/spreadsheets/d/1d0M8CnuRBLvhLU5R8vq_rfc4jLkrxcsgKITfodm4U7s/edit#gid=0";
     // Logger.log(checkStringForSubstring("google", url));
 }
+
+/////////////
